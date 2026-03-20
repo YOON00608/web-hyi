df9ba2508b0f6803bfe5b6f26d3764e3258208ca
윤혜리 <cakesopdj@gmail.com>
2026-03-20 17:13:08 +0900

    Update Ex1_Variable.java

--- java/ex_0320/src/ex_variable/Ex1_Variable.java
+++ java/ex_0320/src/ex_variable/Ex1_Variable.java
@@ -4,9 +4,17 @@ public class Ex1_Variable {
 
 	public static void main(String[] args) {
 		//sysout 템플릿으로 만들기
-		System.out.println(10);
-		System.out.println(3.14);
-		System.out.println("홍길동");
+		//System.out.println(10);
+		//System.out.println(3.14);
+		//System.out.println("홍길동");
+		
+		int x; //x라는 이름의 정수타입의 변수를 선언
+		
+		x = 10; //변수 x에 10을 대입
+		//수학에서 =  -> 좌변과 우변이 같다는 의미
+		//프로그래밍 언어에서 = -> 우변에 값을 좌변에 대입한다는 의미
+		
+		System.out.println(x);
 		
 	}
 
