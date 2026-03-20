a452b715975f15e15a252349bf2c8fd25fbfaf0b
윤혜리 <cakesopdj@gmail.com>
2026-03-20 17:19:46 +0900

    Update Ex1_Variable.java

--- java/ex_0320/src/ex_variable/Ex1_Variable.java
+++ java/ex_0320/src/ex_variable/Ex1_Variable.java
@@ -14,8 +14,18 @@ public class Ex1_Variable {
 		//수학에서 =  -> 좌변과 우변이 같다는 의미
 		//프로그래밍 언어에서 = -> 우변에 값을 좌변에 대입한다는 의미
 		
-		System.out.println(x);
+		System.out.println(x+1); //일회성 연산
 		
+		//연산 후 x의  값은? -> 10
+		System.out.println(x);	//x의 값은 불변
+		
+		x= 55;	//기본의 값을 버리고 새로운 값을 대입할 수 있다.
+		System.out.println(x);	// -> 55
+		
+		//변수 x의 값에 연산을 한 후 다시 대입하면 연산한 값이 유지가 된다.
+		//값을 유지하기 위해서는 대입해야  함.
+		x = x + 1;
+		System.out.println(x); // -> 56
 	}
 
 }
