8a37db6263dfcb89d9dff8ec8b3fa1855440182e
윤혜리 <cakesopdj@gmail.com>
2026-03-20 17:04:31 +0900

    Create Ex1_Variable.java

---
+++ java/ex_0320/src/ex_variable/Ex1_Variable.java
@@ -0,0 +1,13 @@
+package ex_variable;
+
+public class Ex1_Variable {
+
+	public static void main(String[] args) {
+		//sysout 템플릿으로 만들기
+		System.out.println(10);
+		System.out.println(3.14);
+		System.out.println("홍길동");
+		
+	}
+
+}
