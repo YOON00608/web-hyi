09889cc2db7b98bf10fd573c34de0b2f88916633
윤혜리 <cakesopdj@gmail.com>
2026-03-20 18:23:10 +0900

    Create Ex2_ByteExample.java

---
+++ java/ex_0320/src/ex2_datatype/Ex2_ByteExample.java
@@ -0,0 +1,11 @@
+package ex2_datatype;
+
+public class Ex2_ByteExample {
+	public static void main(String[] args) {
+		// 정수의 경우 해당 자료형이 표현할 수 있는
+		// 범위를 벗어난 데이터를 저장하면
+		// 오버플로우, 언더플로우가 발생할 수 있다.
+		
+		
+	}
+}
