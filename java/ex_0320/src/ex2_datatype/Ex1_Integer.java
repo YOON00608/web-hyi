ab3e8b59e6ce67662b11af05ae491c16bf83d90c
윤혜리 <cakesopdj@gmail.com>
2026-03-20 18:20:37 +0900

    Update Ex1_Integer.java

--- java/ex_0320/src/ex2_datatype/Ex1_Integer.java
+++ java/ex_0320/src/ex2_datatype/Ex1_Integer.java
@@ -5,7 +5,7 @@ public class Ex1_Integer {
 		int var1 = 0b1011; // 2진수
 		int var2 = 0206; // 8진수	
 		int var3 = 365; // 10진수
-		int var4 = 0xB3; // 16진수
+		int var4 = 0xB3; // 16진수 //0~9A~F
 		
 		System.out.println("var1: " + var1); // 11
 		System.out.println("var2: " + var2); // 134
