5ed80b6a37a8f6c37945e5feaaaeb1dfde1a31ba
윤혜리 <cakesopdj@gmail.com>
2026-03-23 14:07:58 +0900

    Demotion(강제형변환)

--- java/ex_0323/src/ex2_casting/Ex2_DemotionExample.java
+++ java/ex_0323/src/ex2_casting/Ex2_DemotionExample.java
@@ -29,5 +29,26 @@ public class Ex2_DemotionExample {
 		// 내가 생각한 것과 다른 결과가 나올 수 있으므로,
 		// 주의해야 한다.
 		
+		// -------------------------------------------
+		
+		// 컴파일 단계에서 연산
+		byte result = 10 + 20;
+		System.out.println("result : " + result);
+		
+		byte v1 = 10;
+		byte v2 = 20;
+		// java에서는 byte, short, char끼리의 연산은
+		// 자동으로 int로 변환이 된다.
+		
+		// 실행 시점에 연산이 된다.
+		// 컴파일러는 확실히 값이 고정된 경우에만 미리 계산한다.
+		byte v3 = (byte)(v1 + v2);
+		int v4 = v1 + v2;
+		
+		System.out.println(v3);
+		System.out.println(v4);
+		
+		
+		
 	}
 }
