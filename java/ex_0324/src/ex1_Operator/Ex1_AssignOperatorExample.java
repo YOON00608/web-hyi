f5112da947eeee0dcb598ad30c923f3b850e002a
윤혜리 <cakesopdj@gmail.com>
2026-03-24 10:18:13 +0900

    Create Ex1_AssignOperatorExample.java

---
+++ java/ex_0324/src/ex1_Operator/Ex1_AssignOperatorExample.java
@@ -0,0 +1,21 @@
+package ex1_Operator;
+
+public class Ex1_AssignOperatorExample {
+	public static void main(String[] args) {
+		
+			// =
+			// 우변에 값을 좌변에 대입한다.
+			
+			int n1 = 10;
+			int n2 = 7;
+			
+			// 복합대입연산자
+			// 산술연사자와 대입연산자가 합쳐진 형
+			// +=, -=, *=, /=, %=
+			
+			int x = 10;
+			
+			x += 3; // x = x + 3;
+			System.out.println(x);
+	}
+}
