11c7f0d3119c02847440e1d5481ff1a9100a7dee
윤혜리 <cakesopdj@gmail.com>
2026-03-26 21:10:12 +0900

    Update StudentMain.java

--- java/ex_0326/src/ex3_class/StudentMain.java
+++ java/ex_0326/src/ex3_class/StudentMain.java
@@ -20,10 +20,18 @@ public class StudentMain {
 //		System.out.println("점수 : " +s1.score);
 		
 		
-		Student s = new Student();
-		Student students = students[3];
+		Student[] students = new Student[3];
 		
+		students[0] = new Student("홍길동", 20, 90);
+		students[1] = new Student("고길동", 30, 80);
+		students[2] = new Student("금길동", 25, 70);
 		
+		System.out.println();
+		for(int i = 0; i < students.length; i++) {
+			System.out.println("이름 : " + students[i].name);
+			System.out.println("나이 : " + students[i].age);
+			System.out.println("성적 : " + students[i].score);
+		}
 		
 		
 		
