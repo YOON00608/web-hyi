a98eb96fef9fe5dd746d8f1dfa99af91893db6c7
윤혜리 <cakesopdj@gmail.com>
2026-03-26 18:30:41 +0900

    Create StudentMain.java

---
+++ java/ex_0326/src/ex3_class/StudentMain.java
@@ -0,0 +1,31 @@
+package ex3_class;
+
+public class StudentMain {
+	public static void main(String[] args) {
+		
+//		Student s = new Student();
+//		
+//		System.out.println("이름 : " + s.name);
+//		System.out.println("나이 : " + s.age);
+//		System.out.println("점수 : " + s.score);
+//		
+//		
+//		Student s1 = new Student();
+//		s1.name = "홍길동";
+//		s1.age = 27;
+//		s1.score = 60;
+//		
+//		System.out.println("이름 : " +s1.name);
+//		System.out.println("나이 : " +s1.age);
+//		System.out.println("점수 : " +s1.score);
+		
+		
+		Student s = new Student();
+		Student students = students[3];
+		
+		
+		
+		
+		
+	}
+}
