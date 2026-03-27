ee5eae3e6e6e045e0c7d930ee8f3638a19b6a618
윤혜리 <cakesopdj@gmail.com>
2026-03-27 17:51:07 +0900

    Update PersonMain.java

--- java/ex_0327/src/ex4_constructor/PersonMain.java
+++ java/ex_0327/src/ex4_constructor/PersonMain.java
@@ -3,7 +3,7 @@ package ex4_constructor;
 public class PersonMain {
 	public static void main(String[] args) {
 		Person p1 = new Person("김자바",27);
-		
+		// 기본생성자랑 같이 쓸 수가 없다.
 		p1.introduce();
 	}
 }
