1204b685b7a773a3a691507201802abb6e1c18d1
윤혜리 <cakesopdj@gmail.com>
2026-03-30 10:13:27 +0900

    Update PersonMain.java

--- java/ex_0327/src/ex4_constructor/PersonMain.java
+++ java/ex_0327/src/ex4_constructor/PersonMain.java
@@ -2,8 +2,11 @@ package ex4_constructor;
 
 public class PersonMain {
 	public static void main(String[] args) {
-		Person p1 = new Person("김자바",27);
+		
 		// 기본생성자랑 같이 쓸 수가 없다.
+		// 매개변수가 정해져 있으면
+		// 호출할 때 값을 꼭 전달해야 한다. 
+		Person p1 = new Person("김자바",27);
 		p1.introduce();
 	}
 }
