6d9012ad60b44d5dfd03acfa962d8d947f574a40
윤혜리 <cakesopdj@gmail.com>
2026-03-30 09:53:45 +0900

    Update Person.java

--- java/ex_0327/src/ex4_constructor/Person.java
+++ java/ex_0327/src/ex4_constructor/Person.java
@@ -6,11 +6,13 @@ public class Person {
 	String name;
 	int age;
 	
+	// 생성자(정의)
 	public Person(String name, int age) {
 		this.name = name;
 		this.age = age;
 	}
 	
+	// 메서드(정의)
 	public void introduce() {
 		System.out.printf("안녕하세요. 저는 %d살 %s입니다.", age,name);
 	}
