d99205ebaabd32a6e7e7e074a850bc989aa77432
윤혜리 <cakesopdj@gmail.com>
2026-03-30 18:30:56 +0900

    Create Pen.java

---
+++ java/ex_0330/src/ex6_inheritance/example/Pen.java
@@ -0,0 +1,15 @@
+package ex6_inheritance.example;
+
+public class Pen {
+	
+	public int amount;
+	
+	public Pen() {}; // 기본 생성자 추가
+	
+	public Pen(int amount) {
+		this.amount = amount;
+	}
+	
+	 
+	
+}
