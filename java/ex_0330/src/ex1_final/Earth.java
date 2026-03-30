35547a011f2236c5e7d9a6859ab8c0dea5be70e1
윤혜리 <cakesopdj@gmail.com>
2026-03-30 10:46:36 +0900

    Create Earth.java

---
+++ java/ex_0330/src/ex1_final/Earth.java
@@ -0,0 +1,12 @@
+package ex1_final;
+
+public class Earth {
+	
+	// 지구 반지름
+	static final double EARTH_RADIUS = 6400;
+	
+	// 지구 둘레(표면적)
+	static final double EARTH_SURFACE_AREA =
+			4 * Math.PI * EARTH_RADIUS * EARTH_RADIUS;
+	
+}
