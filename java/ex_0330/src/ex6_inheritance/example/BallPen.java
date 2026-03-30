c56d319b2f8621fbd8ff5c620ac716b6ac49a5db
윤혜리 <cakesopdj@gmail.com>
2026-03-30 18:30:52 +0900

    Create BallPen.java

---
+++ java/ex_0330/src/ex6_inheritance/example/BallPen.java
@@ -0,0 +1,13 @@
+package ex6_inheritance.example;
+
+public class BallPen extends Pen {
+	
+	public String color;
+	public BallPen() {}; // 기본 생성자 추가
+	
+	public BallPen(String color) {
+		this.color = color;
+	}
+
+	
+}
