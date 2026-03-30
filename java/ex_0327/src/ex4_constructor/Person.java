3e14e104c5afdb654388df6d7244864e355d792d
윤혜리 <cakesopdj@gmail.com>
2026-03-30 10:13:29 +0900

    Update Person.java

--- java/ex_0327/src/ex4_constructor/Person.java
+++ java/ex_0327/src/ex4_constructor/Person.java
@@ -7,6 +7,10 @@ public class Person {
 	int age;
 	
 	// 생성자(정의)
+	// 매개변수와 클래스의 멤버변수와 이름이 같다면
+	// 매개변수의 사용 우선순위가 더 높다.
+	// 생성자(or메서드)를 호출한 객체의 필드임을 명시하기 위해서
+	// this키워드를 사용한다.
 	public Person(String name, int age) {
 		this.name = name;
 		this.age = age;
