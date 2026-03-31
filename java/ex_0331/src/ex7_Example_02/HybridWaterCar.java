ecbed7c0fa6933de28dde8615da73bc84960e23f
윤혜리 <cakesopdj@gmail.com>
2026-03-31 20:45:22 +0900

    Update HybridWaterCar.java

--- java/ex_0331/src/ex7_Example_02/HybridWaterCar.java
+++ java/ex_0331/src/ex7_Example_02/HybridWaterCar.java
@@ -4,13 +4,14 @@ public class HybridWaterCar extends HybridCar {
 
 	int waterGauge;
 	
-	public void waterGauge(int gasGauge, int electricGauge, int waterGauge) {
+	public HybridWaterCar(int gasGauge, int electricGauge, int waterGauge) {
+		super(gasGauge, electricGauge); // 부모(HybridCar) 생성자 호출
 		this.waterGauge = waterGauge;
 	}
 	
 	@Override
 	public void showCurrentGauge() {
-		
+		super.showCurrentGauge();	// 부모의 가스, 전기 출력 기능 재사용
+		System.out.println("잔여 물량 : " + waterGauge);
 	}
-	
 }
