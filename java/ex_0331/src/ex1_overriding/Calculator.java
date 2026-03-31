e861b9719afc02cf8521aad726109db35c843556
윤혜리 <cakesopdj@gmail.com>
2026-03-31 10:58:58 +0900

    Create Calculator.java

---
+++ java/ex_0331/src/ex1_overriding/Calculator.java
@@ -0,0 +1,14 @@
+package ex1_overriding;
+
+// 부모 클래스
+public class Calculator {
+	
+	// 반지름을 외부에서 받아 원의 넓이를 반환하는 areaCircle 메서드 작성하기
+	
+	// 메서드의 구조
+	// 접근제한자 반환타입 메서드명(매개변수){}
+	public double areaCirclr(double r) {
+		System.out.println("Calcuator 객체의 areaCircle() 실행");
+		return  3.14 * r * r;
+	}
+}
