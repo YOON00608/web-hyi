391adf8e7dfcc56981a677b2966bd662bdbb170d
윤혜리 <cakesopdj@gmail.com>
2026-03-31 17:43:30 +0900

    Update Main.java

--- java/ex_0331/src/ex5_polymorphism/exam04/Main.java
+++ java/ex_0331/src/ex5_polymorphism/exam04/Main.java
@@ -3,6 +3,7 @@ package ex5_polymorphism.exam04;
 public class Main {
 	public static void main(String[] args) {
 		
+		// 추상클래스는 객체를 직접 생성할 수 없다.
 		Zoo zoo = new Zoo();
 		
 		zoo.printSound(new Dog());
