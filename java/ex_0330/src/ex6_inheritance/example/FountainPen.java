f7326e66bf19509665aa0920498a909f18222ee4
윤혜리 <cakesopdj@gmail.com>
2026-03-31 09:36:08 +0900

    Update FountainPen.java

--- java/ex_0330/src/ex6_inheritance/example/FountainPen.java
+++ java/ex_0330/src/ex6_inheritance/example/FountainPen.java
@@ -1,11 +1,12 @@
 package ex6_inheritance.example;
 
-public class FountainPen extends BallPen {
-	
-	public int refill;
-	
-	public FountainPen(int refill) {
-		this.refill = refill;
+public class FountainPen extends BallPen {	
+	public FountainPen (int amount, String color) {
+		super();
 	}
 	
+	public void refill(int n) {
+		this.amount = n;
+		System.out.println("잉크를 " + n + "만큼 충전했습니다.");
+	}
 }
