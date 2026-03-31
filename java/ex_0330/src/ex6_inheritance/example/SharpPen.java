df8d9f8ea5a0d7e30511edaab8fd565de4ac83c7
윤혜리 <cakesopdj@gmail.com>
2026-03-31 10:28:29 +0900

    Update SharpPen.java

--- java/ex_0330/src/ex6_inheritance/example/SharpPen.java
+++ java/ex_0330/src/ex6_inheritance/example/SharpPen.java
@@ -2,11 +2,18 @@ package ex6_inheritance.example;
 
 public class SharpPen extends Pen {
 	
-	public double width;
+	private int width;
 	
-	public SharpPen(int width) {
-		super();
+	public SharpPen(int width, int amount) {
+		super(amount);
 		this.width = width;
 	}
 	
+	public void setWidth(int width) {
+		this.width = width;
+	}
+	
+	public int getWidth() {
+		return width;
+	}
 }
