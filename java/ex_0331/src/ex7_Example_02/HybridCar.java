71374b35bd3266e76bfa9509bd7f4c099d697dd3
윤혜리 <cakesopdj@gmail.com>
2026-03-31 20:45:20 +0900

    Update HybridCar.java

--- java/ex_0331/src/ex7_Example_02/HybridCar.java
+++ java/ex_0331/src/ex7_Example_02/HybridCar.java
@@ -1,15 +1,17 @@
 package ex7_Example_02;
 
 public class HybridCar extends Car {
+	
 	int  electricGauge;
 	
-	public void electricGauge(int gasGauge,int electricGauge) {
+	public HybridCar(int gasGauge, int electricGauge) {
+		super(gasGauge); // 부모 생성자 호출
 		this.electricGauge = electricGauge;
 	}
 	
 	@Override
 	public void showCurrentGauge() {
-		System.out.println(electricGauge);
+		super.showCurrentGauge(); // 부모의 가스 출력 기능 재사용
+		System.out.println("잔여 전기량 : " + electricGauge);
 	}
-	
 }
