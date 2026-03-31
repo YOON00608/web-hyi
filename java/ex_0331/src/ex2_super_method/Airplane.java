46d05cd7c16ce157c43179da1de5dd5cdae625e6
윤혜리 <cakesopdj@gmail.com>
2026-03-31 14:15:47 +0900

    Create Airplane.java

---
+++ java/ex_0331/src/ex2_super_method/Airplane.java
@@ -0,0 +1,17 @@
+package ex2_super_method;
+
+public class Airplane {
+	
+	// 메서드
+	public void land() {
+		System.out.println("착륙합니다.");
+	}
+	
+	public void fly() {
+		System.out.println("일반 비행합니다.");
+	}
+	
+	public void takeOff() {
+		System.out.println("이륙합니다.");
+	}
+}
