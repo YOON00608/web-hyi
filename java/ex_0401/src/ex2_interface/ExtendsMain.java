08963a507496ac7b7f5062d6ceb7e0bbaef8b713
윤혜리 <cakesopdj@gmail.com>
2026-04-01 15:38:37 +0900

    Update ExtendsMain.java

--- java/ex_0401/src/ex2_interface/ExtendsMain.java
+++ java/ex_0401/src/ex2_interface/ExtendsMain.java
@@ -5,6 +5,15 @@ public class ExtendsMain {
 		
 		InterfaceCImpl impl = new InterfaceCImpl();
 		
+		// 인터페이스의 타입 변환
+		// 인터페이스와 구현클래스 사이에서 발생한다.
+		
+		// 자동 타입변환
+		// 구현 객체 EX) InterfaceCImpl impl = new InterfaceCImpl();
+		// 를 인터페이스 타입변수에 대입할 때 발생한다.
+		
+		// 강제 타입변환
+		
 		InterfaceA ia = impl;
 		ia.methodA();
 		// B, C는 안 됨.
