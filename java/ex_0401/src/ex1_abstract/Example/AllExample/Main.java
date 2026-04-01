2ef9470fb0c8ea14ca54d07db157ad8df6ea490d
윤혜리 <cakesopdj@gmail.com>
2026-04-01 11:39:03 +0900

    Create Main.java

---
+++ java/ex_0401/src/ex1_abstract/Example/AllExample/Main.java
@@ -0,0 +1,13 @@
+package ex1_abstract.Example.AllExample;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		// 부모 타입 배열 사용하기
+		// 반복문(for)을 사용하여 모든 객체의 printFare()를 호출하시오.
+		Transport[] t = {new Bus("버스",1200), new Taxi("택시",3000,10,100)};
+		for(Transport transport : t) {
+			transport.printFare(50);
+		}
+	}
+}
