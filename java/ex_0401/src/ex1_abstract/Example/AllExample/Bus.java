7e1e918fadf85cbdbb44795ceb4b713a8b927468
윤혜리 <cakesopdj@gmail.com>
2026-04-01 12:00:37 +0900

    Update Bus.java

--- java/ex_0401/src/ex1_abstract/Example/AllExample/Bus.java
+++ java/ex_0401/src/ex1_abstract/Example/AllExample/Bus.java
@@ -3,6 +3,10 @@ package ex1_abstract.Example.AllExample;
 public class Bus extends Transport {
 	
 	// Transport의 생성자를 super로 받는다.
+	// 상속을 받으면 자식객체가 만들어질 때
+	// 부모생성자가 먼저 호출이 된다.
+	// 그래서 super()가 생략이 되어있는데
+	// 부모생성자에 매개변수가 들어있으면 명시해줘야 한다.
 	public Bus(String name, int baseFare) {
 		super(name, baseFare);
 	}
