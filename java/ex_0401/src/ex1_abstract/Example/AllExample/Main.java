6d518f482e968015050a1b7d1183ff9e8eb46dab
윤혜리 <cakesopdj@gmail.com>
2026-04-01 12:09:47 +0900

    Update Main.java

--- java/ex_0401/src/ex1_abstract/Example/AllExample/Main.java
+++ java/ex_0401/src/ex1_abstract/Example/AllExample/Main.java
@@ -5,9 +5,12 @@ public class Main {
 		
 		// 부모 타입 배열 사용하기
 		// 반복문(for)을 사용하여 모든 객체의 printFare()를 호출하시오.
-		Transport[] t = {new Bus("버스",1200), new Taxi("택시",3000,10,100)};
+		Transport[] t = {new Bus("버스",1200),
+						 new Taxi("택시",3000,10,100),
+						 new AirPlane("비행기",1200,1000,2000)};
 		for(Transport transport : t) {
 			transport.printFare(50);
+			System.out.println("--------");
 		}
 	}
 }
