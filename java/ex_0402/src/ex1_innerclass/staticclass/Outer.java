820e0b81a27f024e9eaa8a0721a37769c8cdadd5
윤혜리 <cakesopdj@gmail.com>
2026-04-02 11:22:23 +0900

    Create Outer.java

---
+++ java/ex_0402/src/ex1_innerclass/staticclass/Outer.java
@@ -0,0 +1,20 @@
+package ex1_innerclass.staticclass;
+
+public class Outer {
+	
+	int a = 10; // 일반 필드(=멤버 변수)
+	static int b = 20; // 정적 필드(=클래스 변수)
+	
+	// 바깥클래스와 완전히 단절된 것은 아니고, 필요하면 객체를 전달받아 사용할 수 있다.
+	static  class Inner{
+		// 정적 내부 클래스는 Outer 객체에 연결되어 있지 않기 때문이다.
+		void print() {
+			// System.out.println(a); -> x, 일반 필드는 오류
+			System.out.println(b);
+		}
+		
+		void print(Outer outer) {
+			System.out.println(outer.a); // o
+		}
+	}
+}
