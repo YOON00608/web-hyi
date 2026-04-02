f281e33da6ced68408cf30c1ade6ca4a0710336b
윤혜리 <cakesopdj@gmail.com>
2026-04-02 13:39:53 +0900

    Create Main.java

---
+++ java/ex_0402/src/ex1_innerclass/exam/Main.java
@@ -0,0 +1,12 @@
+package ex1_innerclass.exam;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		RemoteControl rc = new RemoteControl();
+		RemoteControl.Button button = rc.new Button();
+		
+		button.press();
+		System.out.println("현재 전원 상태 : " + rc.power);	
+	}
+}
