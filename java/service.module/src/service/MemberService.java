e477b5e1b083b9ca84a786f5bf6ce916634f362e
윤혜리 <cakesopdj@gmail.com>
2026-04-03 10:44:01 +0900

    Update MemberService.java

--- java/service.module/src/service/MemberService.java
+++ java/service.module/src/service/MemberService.java
@@ -8,4 +8,19 @@ public class MemberService {
 		System.out.println("회원 나이 : " + member.getAge());
 		System.out.println("회원 등록이 완료되었습니다.");
 	}
+	
+	// 성인인지 판단하는 CheckAdult() 메서드 만들기
+	// 20세 이상이면 "성인 회원입니다."
+	// 아니며 "미성년회원입니다."
+	
+	public void CheckAdult(Member member) {
+		
+		if(member.getAge() >= 20) {
+			System.out.println("성인 회원입니다.");
+		}else {
+			System.out.println("미성년 회원입니다.");
+		}
+		
+	}
+	
 }
