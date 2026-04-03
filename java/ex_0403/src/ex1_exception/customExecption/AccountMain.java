cedbe65ab5e43ecc5b686188241adff926716b52
윤혜리 <cakesopdj@gmail.com>
2026-04-03 15:48:14 +0900

    Create AccountMain.java

---
+++ java/ex_0403/src/ex1_exception/customExecption/AccountMain.java
@@ -0,0 +1,22 @@
+package ex1_exception.customExecption;
+
+public class AccountMain {
+	public static void main(String[] args) {
+		
+		Account a = new Account();
+		
+		a.deposit(100000);
+		System.out.println("예금액 : " + a.getBalance());
+		
+		try {
+			a.withdraw(150000);
+		} catch (InsufficientException e) {
+			String message = e.getMessage();
+			System.out.println(message);
+		
+		}
+		
+		
+		
+	}
+}
