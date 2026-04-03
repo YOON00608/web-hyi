4d00f6d559c2f9087d3262ef4616ea7a338526e5
윤혜리 <cakesopdj@gmail.com>
2026-04-03 15:12:38 +0900

    Create Converter.java

---
+++ java/ex_0403/src/ex1_exception/tyhrowsexample/Converter.java
@@ -0,0 +1,10 @@
+package ex1_exception.tyhrowsexample;
+
+public class Converter {
+	
+	public int toInt(String str) throws Exception {
+		
+		// Integer.parseInt(문자열);
+		return Integer.parseInt(str);
+	}
+}
