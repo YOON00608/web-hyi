acfc39414aef1731e8fe69ced37d154420ac0304
윤혜리 <cakesopdj@gmail.com>
2026-04-03 18:11:29 +0900

    Create Member.java

---
+++ java/ex_0403/src/ex2_object/exam/Member.java
@@ -0,0 +1,23 @@
+package ex2_object.exam;
+
+public class Member {
+	
+	private String id;
+	private String name;
+	
+	public Member(String id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if(obj instanceof Member target) {
+			if(id.equals(target.id)) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+}
