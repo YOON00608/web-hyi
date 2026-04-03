9520d364b91267733c0817f1e4d94fd4197bf21e
윤혜리 <cakesopdj@gmail.com>
2026-04-03 14:44:16 +0900

    Update Exam.java

--- java/ex_0403/src/ex1_exception/Exam.java
+++ java/ex_0403/src/ex1_exception/Exam.java
@@ -1,5 +1,6 @@
 package ex1_exception;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Exam {
@@ -9,12 +10,39 @@ public class Exam {
 		// 키보드에서 정수를 입력받고, 정수 이외의 값이 입력되었다면
 		// "정수만 입력가능" 메시지 출력하기
 		Scanner sc = new Scanner(System.in);
+		//try {
+		//	System.out.print("정수 입력 : ");
+		//	int num = sc.nextInt();
+		//	System.out.println("결과 :  ");
+		//} catch (Exception e) {
+		//	System.out.println("정수만 입력가능");
+		//}
+		
+		String[] fruits = {"사과","바나나","포도","복숭아"};
+		// 인덱스를 입력받는다.
+		// 해당위치의 과일을 출력한다.
+		// 숫자가 아닌 값을 입력하면 "숫자를 입력해야 합니다." 출력
+		// 범위를 벗어난 인덱스를 입력하면 "존재하지 않는 인덱스입니다." 출력하기
+		
 		try {
-			System.out.print("정수 입력 : ");
-			int num = sc.nextInt();
-			System.out.println("결과 :  ");
+			
+			System.out.print(" 입력 : ");
+			String input = sc.nextLine();
+			
+			int index = Integer.parseInt(input);
+			System.out.println("선택한 과일: " + fruits[index]);
+			
+		} catch (InputMismatchException e) {
+			System.out.println("숫자를 입력해야 합니다.");
+		} catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println("존재하지 않는 인덱스입니다.");
 		} catch (Exception e) {
-			System.out.println("정수만 입력가능");
+			System.out.println("실행 오류입니다.");
 		}
+		
+		
+		
+		
+		
 	}
 }
