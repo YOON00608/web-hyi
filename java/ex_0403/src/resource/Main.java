2aad2c06ab87adf2dc00c1814739f041f397387d
윤혜리 <cakesopdj@gmail.com>
2026-04-03 14:44:20 +0900

    Create Main.java

---
+++ java/ex_0403/src/resource/Main.java
@@ -0,0 +1,23 @@
+package resource;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		try(MyResource res = new MyResource("A")) {
+			String data = res.read1();	// "100";
+			int value = Integer.parseInt(data);
+			
+		} catch (Exception e) {
+			System.out.println("예외 처리 : " + e.getMessage());
+		}
+		
+		try(MyResource res = new MyResource("A")) {
+			String data = res.read2();	// "abc";
+			int value = Integer.parseInt(data);
+			
+		} catch (Exception e) {
+			System.out.println("예외 처리 : " + e.getMessage());
+		}
+		
+	}
+}
