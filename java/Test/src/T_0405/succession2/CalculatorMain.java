2228e23d8f08cc5e2776f3e75e1c9a14bacf3774
윤혜리 <cakesopdj@gmail.com>
2026-04-05 17:06:20 +0900

    Create CalculatorMain.java

---
+++ java/Test/src/T_0405/succession2/CalculatorMain.java
@@ -0,0 +1,13 @@
+package T_0405.succession2;
+
+public class CalculatorMain {
+	public static void main(String[] args) {
+		
+		Calculator c = new Calculator();
+		System.out.println(c.areaCircle(5)); 
+		
+		Computer c2 = new Computer();
+		System.out.println(c2.areaCircle(5));
+		
+	}
+}
