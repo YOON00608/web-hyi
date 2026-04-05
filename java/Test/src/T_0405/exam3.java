f9112e127879f0b76e4872dae3a1b73879648e6d
윤혜리 <cakesopdj@gmail.com>
2026-04-05 16:32:20 +0900

    Create exam3.java

---
+++ java/Test/src/T_0405/exam3.java
@@ -0,0 +1,15 @@
+package T_0405;
+
+public class exam3 {
+	
+	// 넓이를 구하는 메서드를 오버로딩하시오.
+	
+	public int area(int x, int y) {
+		return x * y;
+	}
+	
+	public int area(int x) {
+		return x * x;
+	}
+	
+}
