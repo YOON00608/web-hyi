0ceb568af8e4365a054efb05ab26c20d3b58669d
윤혜리 <cakesopdj@gmail.com>
2026-04-05 16:32:17 +0900

    Create exam1.java

---
+++ java/Test/src/T_0405/exam1.java
@@ -0,0 +1,24 @@
+package T_0405;
+
+public class exam1 {
+
+		// 이름과 나이를 초기화하는 생성자를 작성하시오.
+		
+		String name;
+		int age;
+
+		
+		public exam1(String name, int age) {
+			this.name = name;
+			this.age = age;
+		}
+		
+		// 두 수를 더해서 반환하는 메서드를 작성하시오.
+		
+		public int add(int a, int b) {
+			return a + b;
+		}
+		
+		
+		
+}
