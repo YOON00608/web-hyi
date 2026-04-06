b80fdb2460d043eea7841cc022c1059ee792f4d4
윤혜리 <cakesopdj@gmail.com>
2026-04-06 14:48:49 +0900

    Create MathExample.java

---
+++ java/ex_0406/src/ex5_Math/MathExample.java
@@ -0,0 +1,40 @@
+package ex5_Math;
+
+public class MathExample {
+	public static void main(String[] args) {
+		
+		// ceil() : 올림
+		double v1 = Math.ceil(5.3);
+		System.out.println(v1); // 6.0
+		
+		// floor() : 내림
+		double v2 = Math.floor(5.3);
+		System.out.println(v2); // 5.0
+		
+		// abs() : 절대값
+		double v3 = Math.abs(-10);
+		System.out.println(v3); // 10.0
+	
+		// max() : 최대값 찾기
+		long v4 = Math.max(3, 7);
+		System.out.println("더 큰 수 : " + v4); // 더 큰 수 : 7
+		
+		// Min() : 최소값 찾기
+		long v5 = Math.min(3, 7);
+		System.out.println("더 작은 수 : " + v5); // 더 작은 수 : 3
+		
+		 int[] arr = {5,2,9,1,7};
+		 // max(), min() 을 활용하여 최대값과 최소값 구하기
+		 // 최대값 : 9, 최소값 : 1
+		int max = arr[0];
+		int min = arr[0];
+		
+		for(int i = 0; i < arr.length; i++) {
+			max = Math.max(max, arr[i]);
+			min = Math.min(min, arr[i]);
+		}
+		System.out.println("최대 값 : " + max);
+		System.out.println("최소 값 : " + min);
+		
+	}
+}
