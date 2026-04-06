6340f12374c8781e6c9b0170dc9e0274372082dd
윤혜리 <cakesopdj@gmail.com>
2026-04-06 14:56:36 +0900

    Update MathExample.java

--- java/ex_0406/src/ex5_Math/MathExample.java
+++ java/ex_0406/src/ex5_Math/MathExample.java
@@ -26,15 +26,29 @@ public class MathExample {
 		 int[] arr = {5,2,9,1,7};
 		 // max(), min() 을 활용하여 최대값과 최소값 구하기
 		 // 최대값 : 9, 최소값 : 1
-		int max = arr[0];
-		int min = arr[0];
+		 int max = arr[0];
+		 int min = arr[0];
 		
-		for(int i = 0; i < arr.length; i++) {
+		 for(int i = 0; i < arr.length; i++) {
 			max = Math.max(max, arr[i]);
 			min = Math.min(min, arr[i]);
-		}
-		System.out.println("최대 값 : " + max);
-		System.out.println("최소 값 : " + min);
+		 }
+		 System.out.println("최대 값 : " + max);
+		 System.out.println("최소 값 : " + min);
+		
+		// round() 반올림
+		// 무조건 소수점 첫째 자리에서 반올림을 한다.
+		System.out.println(Math.round(3.15)); // 3
+		
+		double pi = 3.14159;
+		double result2 = Math.round(pi*100)/100.0;
+		System.out.println(result2); // 3.14
+		
+		// pow() : 제곱
+		System.out.println(Math.pow(2,3)); // 8.0
+		
+		
+		
 		
 	}
 }
