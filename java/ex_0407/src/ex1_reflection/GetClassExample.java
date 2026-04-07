552207e116e48a92fb2ba9967a926cf989d92cc6
윤혜리 <cakesopdj@gmail.com>
2026-04-07 11:08:26 +0900

    Create GetClassExample.java

---
+++ java/ex_0407/src/ex1_reflection/GetClassExample.java
@@ -0,0 +1,36 @@
+package ex1_reflection;
+
+public class GetClassExample {
+	public static void main(String[] args) throws Exception {
+		
+		// Class 클래스
+		// 클래스의 정보 자체를 담고 있는 클래스
+		
+		// 방법1
+		// 클래스명.class : 클래스의 정보 가져오기
+		
+		// Class clazz = Car.class;
+		
+		// 방법2
+		// 경로를 찾아갔는데 클래스가 없을 수도 있기 때문에
+		// ClassNotFoundException 예외가 발생할 수 있는 코드가 된다.
+		//Class clazz = Class.forName("ex1_reflection.Car");
+		
+		// 방법3
+		// 객체를 만들고 getClass() 메서드를 통해서 정보를 얻어온다.
+		Car car = new Car();
+		Class clazz = car.getClass();
+		
+		// getPackageName() : 패키지 정보 읽기
+		System.out.println("패키지 : " + clazz.getPackageName()); // 패키지 : ex1_reflection
+		
+		// getSimpleName() : 패키지를 제외한 클래스 이름
+		System.out.println("클래스 간단 이름 : " + clazz.getSimpleName()); // 클래스 간단 이름 : Car
+		
+		// getName() : 패키지를 포함한 전체 클래스명
+		System.out.println("클래스 전체 이름 : " + clazz.getName()); // 클래스 전체 이름 : ex1_reflection.Car
+		
+		
+		
+	}
+}
