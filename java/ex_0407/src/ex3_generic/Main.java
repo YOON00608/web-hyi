b47771deef7e3df30ff4d7b2e4172b6d03ca0832
윤혜리 <cakesopdj@gmail.com>
2026-04-07 17:47:38 +0900

    Update Main.java

--- java/ex_0407/src/ex3_generic/Main.java
+++ java/ex_0407/src/ex3_generic/Main.java
@@ -27,9 +27,25 @@ public class Main {
 		sample.addElement("This is String", 5);
 		System.out.println(sample.getElement(5));
 		
+		///
+		/// 
 		
+		Printer p = new Printer();
 		
+		// 1. 타입 추론
+		// 제네릭 메서드를 호출할 때 전달한 값으로 타입을 자동 판단한다.
+		p.printValue("안녕하세요");
+		p.printValue(100);
+		p.printValue(3.14);
 		
+		// 2. 명시적으로 타입을 적는 방법
+		p.<String>printValue("안녕");
+		p.<Integer>printValue(100);
+		p.<Double>printValue(3.14);
+		
+		// 3. get 사용
+		System.out.println(p.getValue("자바"));
+		System.out.println(p.getValue(1000));
 		
 	}
 }
