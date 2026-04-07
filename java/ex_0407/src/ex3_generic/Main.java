de59e66f0c9cf57ca9392edf8ba2b4eb63bb3e4a
윤혜리 <cakesopdj@gmail.com>
2026-04-07 16:21:57 +0900

    Create Main.java

---
+++ java/ex_0407/src/ex3_generic/Main.java
@@ -0,0 +1,25 @@
+package ex3_generic;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		// 문자열
+		GenEx<String> v1 = new GenEx<String>();
+		
+		v1.setValue("java");
+		System.out.println(v1.getValue());
+		
+		// 정수
+		GenEx<Integer> v2 = new GenEx<Integer>();
+		
+		v2.setValue(100);
+		System.out.println(v2.getValue());
+
+		// 문자형
+		GenEx<Character> v3 = new GenEx<Character>();
+		
+		v3.setValue('A');
+		System.out.println(v3.getValue());
+	
+	}
+}
