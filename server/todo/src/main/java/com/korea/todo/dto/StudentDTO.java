c76e0882d96a576d598b0ea45a058d87b28347df
윤혜리 <cakesopdj@gmail.com>
2026-07-15 15:06:50 +0900

    Update StudentDTO.java

--- server/todo/src/main/java/com/korea/todo/dto/StudentDTO.java
+++ server/todo/src/main/java/com/korea/todo/dto/StudentDTO.java
@@ -10,7 +10,7 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 public class StudentDTO {
-	private int id;
+	private Long id;
 	private String name;
 	private String course;
 	private String email;
